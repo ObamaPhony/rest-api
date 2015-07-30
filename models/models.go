8e2c08681cc91cc1f1eb6c8a441079a7288dc1d9
Dom Rodriguez (shymega) <shymega@shymega.org.uk>
2015-07-30 11:07:45 +0100

    [Add]: models: Added more loggers, GABS (Dynamic JSON) and functions to return loggers and gabs instance.

--- models/models.go
+++ models/models.go
@@ -2,12 +2,50 @@ package models
 
 import (
 	log "github.com/Sirupsen/logrus"
+	"github.com/jeffail/gabs"
 )
 
 var LogObamaREST = log.WithFields(log.Fields{"App": "ObamaPhony/REST"})
 
+var (
+	LogConfig      = LogObamaREST.WithFields(log.Fields{"Module": "/Config"})
+	LogControllers = LogObamaREST.WithFields(log.Fields{"Module": "/Controllers"})
+	LogExec        = LogObamaREST.WithFields(log.Fields{"Module": "/Exec"})
+	LogSpeech      = LogObamaREST.WithFields(log.Fields{"Module": "/Exec/Speech"})
+)
+
 type SpeechesList struct {
-	ID          int    `json:"id"`
-	SpeechTitle string `json:"speechTitle"`
-	President   string `json:"president"`
+	ID       int        `json:"id"`
+	name     string     `json:"name"`
+	analysis [][]string `json:"analysis"`
+}
+
+func ReturnBaseLogger() *log.Entry {
+	return LogObamaREST
+}
+
+func ReturnLogConfig() *log.Entry {
+	return LogConfig
+}
+
+func ReturnLogControllers() *log.Entry {
+	return LogControllers
+}
+
+func ReturnLogExec() *log.Entry {
+	return LogExec
+}
+
+func ReturnLogSpeech() *log.Entry {
+	return LogSpeech
+}
+
+func GABSParseJSON(json []byte) (*gabs.Container, error) {
+	jsonParsed, err := gabs.ParseJSON(json)
+	if err != nil {
+		return jsonParsed, err
+	}
+
+	// No errors, we're good here.
+	return jsonParsed, nil
 }
