a0dd68112ae1dc3b52535fc371666785ba65c92e
Dom Rodriguez (shymega) <shymega@shymega.org.uk>
2015-12-05 21:44:50 +0000

    Using pilu/traffic for REST server now, but needs to be tweaked!

--- controllers/handlers.go
+++ controllers/handlers.go
@@ -1,16 +1,65 @@
 package controllers
 
 import (
+	"github.com/ObamaPhony/rest-api/exec"
 	"github.com/ObamaPhony/rest-api/models"
-	"github.com/gin-gonic/gin"
+	"github.com/jeffail/gabs"
+	"github.com/pilu/traffic"
+	"io/ioutil"
+	"os"
 )
 
-func SGenListSpeeches(c *gin.Context) {
-	speechesList := models.SpeechesList{
-		ID:          1,
-		SpeechTitle: "I'm a president yay!",
-		President:   "Barack Obama",
-	}
+// GenSpeech does
+func GenSpeech(w traffic.ResponseWriter, r *traffic.Request) {
+	loggers := models.ReturnLoggers()
 
-	c.JSON(200, speechesList)
+	// Create channels.
+	loggers.LogControllers.Debug("Channels being generated.")
+	loggers.LogControllers.Debug("Channels generated.")
+
+	SpeechRequest := r.Body
+
+	// Create config instance
+	content, err := ioutil.ReadFile(os.Getenv("OBAMAP_RESTAPI_CONFIG"))
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(500)
+		w.WriteJSON(models.ErrorJSON{
+			500,
+			err,
+		})
+	}
+	config, err := gabs.ParseJSON(content)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(500)
+		w.WriteJSON(models.ErrorJSON{
+			500,
+			err,
+		})
+	}
+	FileOutputPath := config.Path("speech.fileOutputPath").Data().(string)
+	PythonLocation := config.Path("speech.pythonLocation").Data().(string)
+	FileOutput := config.Path("speech.fileOutput").Data().(bool)
+
+	loggers.LogControllers.Info("Executing Speech Analysis.")
+
+	//exec.SpeechAnalysis(&a, done)
+	result, err := exec.SpeechAnalysis2(exec.Arguments{
+		FileOutputPath: FileOutputPath,
+		ScriptLocation: PythonLocation,
+		FileOutput:     FileOutput,
+		SpeechRequest:  SpeechRequest,
+	})
+
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(500)
+		w.WriteJSON(models.ErrorJSON{
+			500,
+			err,
+		})
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteText(result)
 }
--- controllers/server.go
+++ controllers/server.go
@@ -1,30 +1,47 @@
 package controllers
 
 import (
-	// "github.com/ObamaPhony/rest-api/models"
-	"github.com/Sirupsen/logrus"
-	"github.com/gin-gonic/contrib/ginrus"
-	"github.com/gin-gonic/gin"
+	"errors"
+	"github.com/ObamaPhony/rest-api/models"
+	"github.com/pilu/traffic"
+	"net/http"
 	"sync"
-	"time"
 )
 
-func StartServer(hostname string, wg *sync.WaitGroup, chanErrorResult chan error) {
-	r := gin.Default() // Create a instance of Gin.
+// StartServer starts the REST API. It takes three arguments, a hostname of
+// string type (This should be formatted like '127.0.0.1:8080', or else the
+// system will panic!), a waitgroup from the sync package (this is sent from
+// the server.go file)
+func StartServer(listen string, wg *sync.WaitGroup, chanErrorResult chan error) {
+	loggers := models.ReturnLoggers()
 
-	r.Use(ginrus.Ginrus(logrus.StandardLogger(), time.RFC3339, true)) // Use the Ginrus middleware logger.
-
-	v1 := r.Group("api/v1")
-	{
-		v1.GET("/speechgen/speeches", SGenListSpeeches)
+	// Diagnostics, hostname len == 0?
+	if len(listen) == 0 {
+		err := errors.New("HOSTNAME_DIAGCHECK_LEN_EQ0")
+		loggers.LogControllers.Error("The REST API has been passed a empty hostname string! We can't continue, check the config.json file", "The error message in all it's error type glory", err)
+		panic(err)
 	}
 
-	err := r.Run(hostname)
+	loggers.LogControllers.Debug("Creating a router instance..")
+
+	// Create router instance.
+	router := traffic.New()
+
+	router.Get("/speeches", GenSpeech)
+	router.Post("/speech", GenSpeech)
+
+	loggers.LogControllers.Info("Starting the REST API...")
+	err := http.ListenAndServe(listen, router)
 	if err != nil {
+		loggers.LogControllers.Error("The REST API has encountered a error during listening time.", "The errro rmessage in all it#s error type glory", err)
+
 		chanErrorResult <- err
 		wg.Done()
 	}
 
-	chanErrorResult <- nil
+	// Generally the server is stopped with CTRL-C, but I would like to
+	// implement a kill signal at some point that the server detects
+	// automatically. (TODO)
+
 	wg.Done()
 }
