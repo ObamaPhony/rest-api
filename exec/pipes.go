4bc096c5ba8ca061aa2d448784acb33c559206e3
Dom Rodriguez (shymega) <shymega@shymega.org.uk>
2015-12-05 21:49:11 +0000

    Updated exec/exec.go, but it's a WIP!

--- exec/pipes.go
+++ exec/pipes.go
@@ -2,74 +2,136 @@ package exec
 
 import (
 	"bytes"
+	"encoding/json"
 	"gopkg.in/pipe.v2"
-	"sync"
+	"io"
 )
 
-// SpeechAnalysis does takes three channels, one to return the buffer, one to return any errors and another to let the calling function know when it's done.
-// It also takes a bool variable to signify if we are writing to file from the output of the python script as well as to a variable.
-func SpeechAnalysis(chanBytesBuffer chan *bytes.Buffer, chanErrorResult chan error, file bool, wg *sync.WaitGroup, speechlocationFile string, speechlocationVar string) {
-	switch file {
-	// A true value for file means that we are executing the Python speech-analyisis module with both file output and standard output, but we are putting the standard output
-	// into a k:v store (the output is JSON), or a variable and return it.
-	case true:
-		// Initalize the buffer struct.
+// Arguments is a struct
+type Arguments struct {
+	FileOutputPath string
+	ScriptLocation string
+	FileOutput     bool
+	SpeechRequest  io.ReadCloser
+}
+
+/*// SpeechAnalysis does takes three channels, one to return the buffer, one to return any errors and another to let the calling function know when it's done.*/
+//// It also takes a bool variable to signify if we are writing to file from the output of the python script as well as to a variable.
+//func SpeechAnalysis(a *Arguments, done chan bool) {
+//// If FileOutput is true, start a new goroutine and continue the analysis, and
+//// then end the function.
+//if a.FileOutput == true {
+//// Create a new buffer
+//buffer := new(bytes.Buffer)
+//fileName := a.FileOutputPath + "/" + "speechOutput_" + time.Now().Format(time.RFC3339) + ".json"
+
+//// Create the output file, as specified.
+//file, err := os.Create(fileName)
+//if err != nil {
+//a.ChanError <- err
+//close(done)
+//}
+//defer file.Close()
+
+//// Prepare the speech analysis pipe.
+//p := pipe.Line(
+//pipe.ReadFile("/home/dzrodrig/dev/projects/src/github.com/ObamaPhony/speech-analysis/test-speeches.txt"),
+//pipe.Exec("python3", a.ScriptLocation),
+//)
+//// Execute the pipe.
+//output, err := pipe.CombinedOutput(p)
+//if err != nil {
+//a.ChanError <- err
+//close(done)
+//}
+
+//// Convert the pipe output into a buffer.
+//if err := json.Compact(buffer, output); err != nil {
+//a.ChanError <- err
+//close(done)
+//}
+
+//// Pass the buffer to the channel
+//a.ChanSpeechOutput <- buffer.String()
+
+//// And also output the buffer to the file
+//_, err = file.Write(buffer.Bytes())
+//if err != nil {
+//a.ChanError <- err
+//close(done)
+//}
+
+//close(done)
+//} else if a.FileOutput == false {
+//// Create a new buffer
+//buffer := new(bytes.Buffer)
+
+//// Prepare the speech analysis pipe.
+//p := pipe.Line(
+//pipe.ReadFile("/home/dzrodrig/dev/projects/src/github.com/ObamaPhony/speech-analysis/test-speeches.txt"),
+//pipe.Exec("python3", a.ScriptLocation),
+//)
+
+//// Execute the pipe.
+//_, err := pipe.CombinedOutput(p)
+//if err != nil {
+//a.ChanError <- err
+//close(done)
+//}
+
+//// Pass the buffer to the channel
+//speech := buffer.String()
+//a.ChanSpeechOutput <- speech
+
+//// Done.
+//close(done)
+
+//}
+/*}*/
+
+// SpeechAnalysis2 d
+func SpeechAnalysis2(a Arguments) (string, error) {
+	if a.FileOutput != true {
+		// Output to channel only.
+
+		// Create new bytes buffer.
 		b := &bytes.Buffer{}
-
-		// Create a Pipe instance to pipe to the speech-analysis module.
 		p := pipe.Line(
-			pipe.ReadFile("./test-speeches.txt"), // TODO: Probs should look at functions arguments for this.
-			// pipe.Exec("python", "/home/dzrodrig/ObamaPhony/speech-analysis/analyse-speeches.py"), // The full path should be changed in production!
-			pipe.Exec("python", "/home/dzrodrig/dev/ObamaPhony/speech-analysis/analyse-speeches.py"),
-			pipe.Tee(b),                          // Output to the 'b' buffer.
-			pipe.WriteFile("results.json", 0644), // Also output to the file, we want that to happen!
+			pipe.Read(a.SpeechRequest),
+			pipe.Exec(a.ScriptLocation),
+			pipe.Tee(b),
 		)
-		// Run the Pipe instance.
 		err := pipe.Run(p)
-		// Error handling for the win.
 		if err != nil {
-			// If error detected, send back two channels previously defined, one with the buffer result (most likely corrupted or failed, but let's do it anyway)
-			// and a channel with the error value.
-			chanBytesBuffer <- b
-			chanErrorResult <- err
-			wg.Done() // Because we found a error, we should stop the goroutine prematurely, just to be safe.
-		} else {
-			// If no error detected, send back buffer instance and nil error value.
-			chanBytesBuffer <- b
-			chanErrorResult <- nil
-			wg.Done() // Because we found a error, we should stop the goroutine prematurely, just to be safe.
+			return "error pipe", err
 		}
-		// A false value for file means that we are executing the Python speech-analysis module with just standard output, which we are putting the standard output
-		// into a k:v store (the output is JSON), or a variable and return via channels.
-	case false:
-		// Initialize the buffer struct.
-		b := &bytes.Buffer{}
 
-		// Create a Pipe instance to pipe the speech-analysis module.
-		p := pipe.Line(
-			pipe.ReadFile("./test-speeches.txt"), // TODO: Probs should look at functions arguments for this.
-			// pipe.Exec("python", "/data1/_NASDrive/dzrodrig/Documents/dev/projects/src/github.com/ObamaPhony/speech-analysis/analyse-speeches.py"), // The full path should be changed in production!
-			pipe.Exec("python", "/home/dzrodrig/dev/ObamaPhony/speech-analysis/analyse-speeches.py"),
-			pipe.Tee(b), // Outputting to the 'b' buffer.
-		)
-
-		// Run the Pipe instance.
-		err := pipe.Run(p)
-		// Error handling for the win.
-		if err != nil {
-			// If error detected, send back two channels previously defined, one with the buffer result (most likely corrupted or failed, but let's do it anyway)
-			// and a channel with the error value.
-			chanBytesBuffer <- b
-			chanErrorResult <- err
-			wg.Done() // Because we found a error, we should stop the goroutine prematurely, just to be safe.
-		} else {
-			// If no error detected, send back buffer instance and nil error value.
-			chanBytesBuffer <- b
-			chanErrorResult <- nil
-			wg.Done() // Because we didn't find a error, we should stop the goroutine as expected.
+		stringBuffer := &bytes.Buffer{}
+		if err := json.Compact(stringBuffer, b.Bytes()); err != nil {
+			panic(err)
 		}
+
+		return stringBuffer.String(), nil
 	}
 
-	// We've finished the switch statement now, just in case the logic didn't stop this goroutine (which it should run as!), we are stopping now.
-	defer wg.Done()
+	// Output to channel and file.
+
+	// Create new bytes buffer.
+	b := &bytes.Buffer{}
+	p := pipe.Line(
+		pipe.Read(a.SpeechRequest),
+		pipe.Exec(a.ScriptLocation),
+		pipe.Tee(b),
+	)
+	err := pipe.Run(p)
+	if err != nil {
+		return "error pipe", err
+	}
+
+	stringBuffer := &bytes.Buffer{}
+	if err := json.Compact(stringBuffer, b.Bytes()); err != nil {
+		panic(err)
+	}
+
+	return stringBuffer.String(), nil
 }
