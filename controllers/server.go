608b00587098472383e6e3ee4c3b7513fbd4cb7a
Dom Rodriguez <shymega@shymega.org.uk>
2017-06-03 15:35:43 +0100

    Remove /make endpoint

--- controllers/server.go
+++ controllers/server.go
@@ -11,14 +11,9 @@ func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "Welcome.\n")
 }
 
-func Make(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	fmt.Fprint(w, "<h1>This is a WIP (Work-in-Progress) route.</h1>")
-}
-
 func Server() {
 	router := httprouter.New()
 	router.GET("/", Index)
-	router.GET("/make", Make)
 
 	http.ListenAndServe(":8080", router)
 }
