2c25828ec722154fc8ba6db741dca1d8e37ea89e
Dom Rodriguez <shymega@shymega.org.uk>
2019-07-19 16:13:35 +0100

    Change router to gorilla/mux

--- internal/controllers/server.go
+++ internal/controllers/server.go
@@ -1,18 +1,22 @@
 package controllers
 
 import (
-	"github.com/gin-gonic/gin"
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
 )
 
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("X-Application", "ObamaPhony")
+	w.Header().Add("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(200)
+
+	fmt.Fprintf(w, "{\"result\": \"PONG\"}")
+}
+
 func Server(bindInter string) {
-	gin.SetMode(gin.ReleaseMode)
+	r := mux.NewRouter()
+	r.HandleFunc("/", handleRoot).Methods("GET")
 
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"payload": "PONG",
-		})
-	})
-
-	r.Run(bindInter)
+	http.ListenAndServe(bindInter, r)
 }
